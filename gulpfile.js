3e4290d663317d93d73fa6d1a8abe060327fa7f5
adnanrahic <ado.raha198@gmail.com>
2017-03-27 22:48:01 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+  console.log('Hello World!');
+});
+
+gulp.task('hello', function () {
+  console.log('Hello ');
+});
+
+gulp.task('world', function () {
+  console.log('World!');
+});
+
